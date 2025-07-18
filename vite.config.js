a32fbc9da197c3f3aa083947bbf72ce4daab8d79
Grisha Konshin <grishakonshin123@gmail.com>
2025-07-19 02:24:58 +0500

    site

---
+++ vite.config.js
@@ -0,0 +1,27 @@
+import { defineConfig } from "vite"
+import tailwindcss from "@tailwindcss/vite"
+export default defineConfig({
+    plugins: [tailwindcss()],
+    build: {
+        rollupOptions: {
+            output: {
+                assetFileNames: (assetInfo) => {
+                    // const info = assetInfo.name.split(".")
+                    // const extType = info[info.length - 1]
+                    if (/\.(png|jpe?g|gif|svg|webp|webm|)$/.test(assetInfo.names)) {
+                        return "assets/images/[name].[ext]"
+                    }
+                    if (/\.(css)$/.test(assetInfo.names)) {
+                        return "assets/css/[name].css"
+                    }
+                    if (/\.(woff|woff2|eot|ttf|otf)$/.test(assetInfo.names)) {
+                        return "assets/fonts/[name].[ext]"
+                    }
+                    return "assets/[name].[ext]"
+                },
+                chunkFileNames: "assets/js/[name].js",
+                entryFileNames: "assets/js/[name].js",
+            },
+        },
+    },
+})
