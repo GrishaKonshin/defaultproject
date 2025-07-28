7a258045528a4f800cf5a9f12bbe47e73f3a5f3e
Grisha Konshin <grishakonshin123@gmail.com>
2025-07-28 17:13:23 +0500

    only vue

--- vite.config.js
+++ vite.config.js
@@ -1,28 +1,7 @@
-import { defineConfig } from "vite"
-import tailwindcss from "@tailwindcss/vite"
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+// https://vite.dev/config/
 export default defineConfig({
-    base: "defaultproject",
-    plugins: [tailwindcss()],
-    build: {
-        rollupOptions: {
-            output: {
-                assetFileNames: (assetInfo) => {
-                    // const info = assetInfo.name.split(".")
-                    // const extType = info[info.length - 1]
-                    if (/\.(png|jpe?g|gif|svg|webp|webm|)$/.test(assetInfo.names)) {
-                        return "assets/images/[name].[ext]"
-                    }
-                    if (/\.(css)$/.test(assetInfo.names)) {
-                        return "assets/css/[name].css"
-                    }
-                    if (/\.(woff|woff2|eot|ttf|otf)$/.test(assetInfo.names)) {
-                        return "assets/fonts/[name].[ext]"
-                    }
-                    return "assets/[name].[ext]"
-                },
-                chunkFileNames: "assets/js/[name].js",
-                entryFileNames: "assets/js/[name].js",
-            },
-        },
-    },
+  plugins: [vue()],
 })
